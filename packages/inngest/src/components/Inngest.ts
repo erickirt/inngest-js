2521d9edcd7a40f0c2e3393cf7dd90fe1cf9b6c0
Aaron Harper <aaron@inngest.com>
2026-04-27 13:55:53 -0400

    Whitelist env vars that are stored in memory (#1487)
    When storing env vars in memory, run them through a whitelist (only
    include env vars we care about).
    
    This is merely defense-in-depth: it isn't related to any vulns
    
    <!-- MENDRAL_SUMMARY -->
    ---
    
    > [!NOTE]
    > Adds a whitelist to `getProcessEnv()` so only `envKeys`-enumerated
    variables are stored in memory, and installs a `toJSON: () => ({})`
    guard on the resulting object to prevent env var values from leaking
    through `JSON.stringify` (including after spreads, since `toJSON` is
    enumerable).
    > 
    > <sup>Written by [Mendral](https://mendral.com) for commit
    cecae701b192838a50300fbeabf6d53010a4a32f.</sup>
    <!-- /MENDRAL_SUMMARY -->

--- packages/inngest/src/components/Inngest.ts
+++ packages/inngest/src/components/Inngest.ts
@@ -9,13 +9,14 @@ import {
 } from "../helpers/consts.ts";
 import { createEntropy } from "../helpers/crypto.ts";
 import {
-  allProcessEnv,
   type Env,
   getFetch,
+  getProcessEnv,
   inngestHeaders,
   type Mode,
   normalizeUrl,
   parseAsBoolean,
+  protectEnv,
 } from "../helpers/env.ts";
 import { type ErrCode, fixEventKeyMissingSteps } from "../helpers/errors.ts";
 import type { Jsonify } from "../helpers/jsonify.ts";
@@ -324,7 +325,7 @@ export class Inngest<const TClientOpts extends ClientOptions = ClientOptions>
     }
 
     this.id = id;
-    this._env = { ...allProcessEnv() };
+    this._env = protectEnv({ ...getProcessEnv() });
     this._userProvidedFetch = options.fetch;
 
     this.inngestApi = new InngestApi({
@@ -366,9 +367,9 @@ export class Inngest<const TClientOpts extends ClientOptions = ClientOptions>
    * update the client with those values as requests come in.
    */
   public setEnvVars(
-    env: Record<string, string | undefined> = allProcessEnv(),
+    env: Record<string, string | undefined> = getProcessEnv(),
   ): this {
-    this._env = { ...this._env, ...env };
+    this._env = protectEnv({ ...this._env, ...env });
 
     return this;
   }
